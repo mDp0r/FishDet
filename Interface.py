4b7d0fe38438b4f6eff4a0e999ba6e8c1689da27
Mario <mDp0r@github.com>
2021-01-30 12:48:44 +0100

    First commit

---
+++ Interface.py
@@ -0,0 +1,341 @@
+#ToDo
+#Add Infer part
+#Add cleanup (copy latest model, remove checkpoints)
+#Kleinen kroatischen Datensatz Annos vorbereiten und Viz testen. Auch kmeans laufen lassen für Anchors
+
+#Imports
+import os
+import subprocess
+import argparse
+import sys
+#Main Class
+class FishDet:
+    """
+    Initialisation of Class 
+    _______________________
+    Input Arguments:
+    _______________________
+    args, type namespace, contains parsed arguments, includes following attributes
+        do: type string, only train, eval, infer supported
+        batch_size: Type int /needed for train
+        lr: type float, is "learnrate" /needed for train
+        num_epochs: Type int /needed for train
+        detector: Type String, So far only EfficientDet supported
+        c: Compound param of EfficientDet, should be None if not EfficientDet
+        ds_name: Type String, See folder name in /datasets of EfficientDet Pytorch so far,
+        load_weights: Type String, Default None, otherwise name of .pth File
+        head_only: Type Bool, default True
+        optim: Type String, default adamw, sgd also supported /used for train
+        debug: Type Bool, default False. Used in EfficientDet Vizualisation to draw boxes during training /used for train
+        infer_mode: Type String, default "all", so far "all", "coco" and "viz" supported /needed for inference
+        conf_threshold : Type Float, default 0.05 / Threshold to Filter Bboxes in the postprocessing after predicting / needed for inference
+        path: Type String, default None /needed for inference
+    _______________________
+    Function:
+    _______________________
+    Initializes attributes with Inputs, also calls detector specific init functions.
+    """  
+    def __init__ (self, args):
+        
+        #Set Input as attributes
+        self.do = args.do
+        self.detector = args.detector
+        self.ds_name = args.ds_name
+        self.load_weights = args.load_weights
+        self.ds_name = args.ds_name
+        self.head_only = args.head_only
+        self.batch_size = args.batch_size
+        self.lr = args.lr
+        self.num_epochs = args.num_epochs
+        self.optim = args.optim
+        self.debug = args.debug
+        self.conf_threshold = args.conf_threshold
+        self.infer_mode = args.infer_mode
+        self.path = args.path
+        #Do Initialisation based on detector
+        if self.detector == "EfficientDet":
+            self.__init_EfficientDet(args.c)
+        
+        #elif self.detector == "CSPNet":
+            #self.__init_CSPNet()
+    
+        return
+    
+    """
+    Initialization of EfficientDet
+    _________
+    Function:
+    _________
+    Does EfficientDet specific Initialisation. So far just cds into right dir of
+    EfficientDet implementation and saves compound param to object attribute
+    
+    """
+    def __init_EfficientDet(self, c):
+        
+        os.chdir("Yet-Another-EfficientDet-Pytorch")
+        self.c = c
+        return
+    
+    """
+    Does command according to do attribute.
+    _________
+    Function:
+    _________
+    Can be called outside of class. Does appropriate command. So far just "train" and "eval".
+    specified.
+    
+    """
+    def do_command(self):
+        if self.do == "train":
+            self.__train()
+        if self.do == "eval":
+            self.__eval()
+        elif self.do == "infer":
+            self.__infer()
+        return
+        
+    
+    """
+    train method:
+    _____________
+    Function:
+    _____________
+    Depending on Detector calls right train method
+    
+    """
+    def __train(self):
+        
+        if self.detector == "EfficientDet":
+            self.__train_EfficientDet()
+            
+            
+        #elif self.detector == "CSPNet"
+            #self.__train_CSPNet()
+            
+        return
+    
+    """
+    train Interface function for EfficientDet
+    _________
+    Function:
+    _________
+    Creates Shell Command String from object attributes. Spawns subprocess with 
+    with command.
+    
+    """
+    
+    def __train_EfficientDet(self):
+        self.__os_command(self.__get_EfficientDet_train_cmd())
+        return    
+    
+    
+    """
+    Shell Command Creation for EfficientDet Training
+    ___________
+    Function:
+    ___________
+    Creates Shell Command String from Inputs specified at Object Creation.
+    ___________
+    Outputs:
+    ___________
+    cmd, type String
+    
+    """
+    def __get_EfficientDet_train_cmd(self):
+        
+        cmd = "python -u train.py"
+
+        # training with preset weights
+        if self.load_weights is not None:
+            cmd += " --load_weights weights/" + self.load_weights
+
+        #Add head only option to cmd string
+        if self.head_only is not None:
+            cmd += " --head_only " + str(self.head_only)
+
+        #Add optim to cmd string
+        if self.optim is not None:
+            cmd += " --optim " + str(self.optim)
+        
+        #Add debug option to cmd string
+        if self.debug is not None:
+            cmd+= " --debug " + str(self.debug)
+            
+        #Add rest of arguments
+        cmd += " --batch_size " + str(self.batch_size) + " --lr " + str(self.lr) + \
+        " --num_epochs " + str(self.num_epochs) + " -p " + str(self.ds_name) + \
+        " --num_workers 0" + " -c " + str(self.c) \
+        + "--save_interval " + str(sys.maxsize) 
+    
+        return cmd
+        
+    """
+    Function for spawning subprocess with command
+    _________
+    Inputs:
+    _________
+    cmd: Type String, command you would enter in console
+    _________
+    Function:
+    _________
+    Uses subprocess library to spawn new subprocess on os with command.
+    Also prints in the executing console the Output of the executed process.
+    
+    """
+    def __os_command(self, cmd): 
+        print("Command used in subprocess: "+cmd)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, text=True, \
+                            encoding = "utf8")
+        while True:
+            inchar = p.stdout.read(1)
+            if inchar: #neither empty string nor None
+                print(str(inchar), end='') #or end=None to flush immediately
+            else:
+                print('') #flush for implicit line-buffering
+                break
+        return
+    
+        """
+    eval method:
+    _____________
+    Function:
+    _____________
+    Depending on Detector calls right eval method
+    
+    """
+    def __eval(self):
+        
+        if self.detector == "EfficientDet":
+            self.__eval_EfficientDet()
+            
+            
+        #elif self.detector == "CSPNet"
+            #self.__eval_CSPNet()
+            
+        return
+    
+    """
+    Evaluation function for EfficientDet
+    __________
+    Function:
+    __________
+    Uses get_EfficientDet_eval_command method to get the right eval command considering Input attributes. Then calls __os_command function
+    to spawn subprocess with function.
+    
+    """
+    def __eval_EfficientDet(self):
+        self.__os_command(self.__get_EfficientDet_eval_command())
+        return
+    
+    """
+    Function to create coco eval command for EfficientDet Repo. 
+    _________
+    Function:
+    Appends input parameters according to specified attributes. Will be used to call in os_command.
+    _________
+    Output:
+    _________
+    Type: String
+    
+    """
+    def __get_EfficientDet_eval_command(self): 
+        return "python -u coco_eval.py" + " -c " + str(self.c)+ " -p " + str(self.ds_name) + " -w weights/" + str(self.load_weights)
+    
+    """
+    infer method:
+    _____________
+    Function:
+    _____________
+    Depending on Detector calls right train method
+    
+    """
+    def __infer(self):
+        
+        if self.detector == "EfficientDet":
+            self.__infer_EfficientDet()
+            
+            
+        #elif self.detector == "CSPNet"
+            #self.__infer_CSPNet()
+            
+        return
+    
+    """
+    Infer function for EfficientDet
+    __________
+    Function:
+    __________
+    Uses get_EfficientDet_infer_command method to get the right eval command considering Input attributes. Then calls __os_command function
+    to spawn subprocess with function.
+    
+    """
+    def __infer_EfficientDet(self):
+        self.__os_command(self.__get_EfficientDet_infer_command())
+        return
+    
+    """
+    Function to create infer command for EfficientDet Repo. 
+    _________
+    Function:
+    Appends input parameters according to specified attributes. Will be used to call in os_command.
+    _________
+    Output:
+    _________
+    Type: String
+    
+    """
+    def __get_EfficientDet_infer_command(self): 
+        return "python -u infer.py" + " --c " + str(self.c)+ " --p " + str(self.ds_name) + " --w weights/" + str(self.load_weights) + \
+    " --mode " + str(self.infer_mode) + " --path " + str(self.path) + " --conf_threshold " + str(self.conf_threshold)
+    
+    
+#Other functions
+
+"""
+Function to get inputs from command line
+_________
+Function:
+_________
+Creates and specifies parser for this script. Parses Items.
+_________
+Output:
+________
+Type: namespace
+"""
+def get_inputs():
+    #Initialize parser
+    parser = argparse.ArgumentParser(description='Interface Script for Fish Detection')
+    #Add Arguments
+    parser.add_argument("--do", action="store", dest = "do", default = None)
+    parser.add_argument("--batch_size", action="store", dest = "batch_size", type=int, \
+default = None)
+    parser.add_argument("--lr", action="store", dest = "lr", type=float, default = None)
+    parser.add_argument("--num_epochs", action="store", dest = "num_epochs", type=int,\
+default = None)
+    parser.add_argument("--detector", action="store", dest = "detector", default = "EfficientDet")
+    parser.add_argument("--c", action="store", dest = "c", type=int, default = None)
+    parser.add_argument("--ds_name", action="store", dest = "ds_name", default = None)
+    parser.add_argument("--load_weights", action="store", dest = "load_weights", \
+default = None)
+    parser.add_argument("--head_only", action="store", dest="head_only", type=bool, \
+default = None)
+    parser.add_argument("--optim", action="store", dest="optim", type=str, \
+default = None)
+    parser.add_argument("--debug", action="store", dest="debug", type=bool, \
+default = None)
+    parser.add_argument("--infer_mode", action="store", dest="infer_mode", type=str, \
+default = "all")
+    parser.add_argument("--path", action="store", dest="path", type=str, \
+default = None)
+    parser.add_argument ("--conf_threshold", action = "store", dest ="conf_threshold", type=float, default = 0.05)
+    #Return parsed object
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    #Parse inputs
+    inputs=get_inputs()
+    #Create Object
+    myFishDet = FishDet(inputs)
+    #Do Command
+    myFishDet.do_command()
+    
---
+++ util_files/infer.py
@@ -0,0 +1,331 @@
+import os
+import datetime
+import json
+import os
+
+import argparse
+import torch
+import yaml
+from tqdm import tqdm
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+from backbone import EfficientDetBackbone
+from efficientdet.utils import BBoxTransform, ClipBoxes
+from utils.utils import preprocess, invert_affine, postprocess, boolean_string
+
+import pandas as pd
+from PIL import Image
+import numpy as np
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from shutil import copy2
+
+def parse_arguments():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--p', type=str, default='coco', help='project file that contains parameters')
+    ap.add_argument('--c',  type=int, default=0, help='coefficients of efficientdet')
+    ap.add_argument('--w',  type=str, default=None, help='/path/to/weights')
+    ap.add_argument('--nms_threshold', type=float, default=0.5, help='nms threshold, don\'t change it if not for testing purposes')
+    ap.add_argument('--cuda', type=boolean_string, default=True)
+    ap.add_argument('--device', type=int, default=0)
+    ap.add_argument('--float16', type=boolean_string, default=False)
+    ap.add_argument('--mode', type=str, default='all', help='viz:visualize pics, coco:create coco annos, lost: create lost annos, all:does everything')
+    ap.add_argument('--path', type=str, default=None, help='Path to imgs in Yet-Another-EfficientDet Folder')
+    ap.add_argument('--conf_threshold', type=float, default=0.05, help="Filters BBoxes with Confidence smaller this value")
+    #ap.add_argument('--override', type=boolean_string, default=True, help='override previous bbox results file if exists')
+    return ap.parse_args()
+
+class FishPred:
+    
+    def __init__(self, args):
+        self.p = args.p
+        self.c = args.c
+        self.weights = args.w
+        self.nms_threshold = args.nms_threshold
+        self.cuda = args.cuda
+        self.float16 = args.float16
+        self.device = args.device
+        self.mode = args.mode
+        self.session = self.__create_dirs()
+        self.impath = args.path if args.path else "datasets/"+ args.p +"/val/"
+        self.input_sizes =[512, 640, 768, 896, 1024, 1280, 1280, 1536, 1536]
+        self.p_params = yaml.safe_load(open('projects/'+self.p+".yml"))
+        self.conf_threshold = args.conf_threshold
+        self.__save_configs()
+        
+        return
+    
+    def __create_dirs(self):
+        print("Creating directories")
+        if "inference" not in os.listdir():
+            os.mkdir("inference")
+        session_path = "inference/"+datetime.datetime.today().strftime("%Y%m%d_%H%M%S")+"/"
+        os.mkdir(session_path)
+        
+        if (self.mode == "all") or (self.mode == "viz"):
+            os.mkdir(session_path+"imgs_with_bboxes")
+            
+        if (self.mode == "all") or (self.mode == "coco"):
+            os.mkdir(session_path+"coco_annos/")
+            os.mkdir(session_path+"coco_annos/annotations/")
+            os.mkdir(session_path+"coco_annos/train/")
+            os.mkdir(session_path+"coco_annos/val/")
+        
+        
+        return session_path
+    
+    
+    
+    def __save_configs(self):
+        print("Writing Config file")
+        with open(self.session+"Configs.txt", "w") as text_file:
+            print(f"Weights: {self.weights}\nProject: {self.p}\nImgpath: {self.impath}\nmode: {self.mode}", file=text_file)
+        
+        
+        return
+    
+    def do_command(self):
+        print("Starting predictions....")
+        self.__predict()
+        print("Mode "+self.mode+" seletected")
+        if self.mode == "all":
+            print("Writing COCO Annos")
+            self.__write_coco()
+#             print("Writing LOST Annos")
+#             self.__write_lost()
+            print("Saving Images with Bboxes")
+            self.__viz()
+            
+        elif self.mode =="coco":
+            print("Writing COCO Annos")
+            self.__write_coco()
+            
+#         elif self.mode =="lost":
+#             print("Writing LOST Annos")
+#             self.__write_lost()
+            
+        elif self.mode == "viz":
+            print("Saving Images with Bboxes")
+            self.__viz()
+        else:
+            print("Error. No valid mode specified")
+        return
+    
+    def __predict(self):
+        
+        
+        
+        model = EfficientDetBackbone(compound_coef=self.c, num_classes=len(self.p_params["obj_list"]),
+                                         ratios=eval(self.p_params['anchors_ratios']), scales=eval(self.p_params['anchors_scales']))
+            
+        model.load_state_dict(torch.load(self.weights, map_location=torch.device('cpu')))
+
+        model.requires_grad_(False)
+        if self.cuda:
+            model.cuda(self.device)
+            if self.float16:
+                model.half()
+        model.eval()
+        imgs = [self.impath+x for x in os.listdir(self.impath)]
+        results = []
+
+        regressBoxes = BBoxTransform()
+        clipBoxes = ClipBoxes()
+
+        for image in tqdm(imgs):
+
+            ori_imgs, framed_imgs, framed_metas = preprocess(image, max_size=self.input_sizes[self.c])
+            x = torch.from_numpy(framed_imgs[0])
+
+            if self.cuda:
+                x = x.cuda(self.device)
+                if self.float16:
+                    x = x.half()
+                else:
+                    x = x.float()
+            else:
+                x = x.float()
+
+            x = x.unsqueeze(0).permute(0, 3, 1, 2)
+            features, regression, classification, anchors = model(x)
+
+            preds = postprocess(x,
+                                anchors, regression, classification,
+                                regressBoxes, clipBoxes,
+                                self.conf_threshold, self.nms_threshold)
+            if not preds:
+                continue
+
+            preds = invert_affine(framed_metas, preds)[0]
+
+            scores = preds['scores']
+            class_ids = preds['class_ids']
+            rois = preds['rois']
+
+            if rois.shape[0] > 0:
+                # x1,y1,x2,y2 -> x1,y1,w,h
+                rois[:, 2] -= rois[:, 0]
+                rois[:, 3] -= rois[:, 1]
+
+                bbox_score = scores
+
+                for roi_id in range(rois.shape[0]):
+                    score = float(bbox_score[roi_id])
+                    label = int(class_ids[roi_id])
+                    box = rois[roi_id, :]
+
+                    image_result = {
+                        'img_pth': image,
+                        'category_id': label + 1,
+                        'score': float(score),
+                        'bbox': box.tolist(),
+                    }
+
+                    results.append(image_result)
+        
+        self.results = pd.DataFrame(results)
+        self.results["bbox"] =self.results["bbox"].apply\
+    (lambda x: np.rint(x,np.zeros(4,int),casting='unsafe'))
+        self.results ["x"] = self.results["bbox"].apply(lambda x : x[0])
+        self.results ["y"] = self.results["bbox"].apply(lambda x : x[1])
+        self.results ["width"] = self.results["bbox"].apply(lambda x : x[2])
+        self.results["height"] = self.results["bbox"].apply(lambda x : x[2])
+       
+    def __viz(self):
+        for temp_path in tqdm(self.results["img_pth"].unique()):
+            self.__viz_annos_per_img(temp_path, self.results.copy().loc[self.results["img_pth"]==temp_path], self.session+"imgs_with_bboxes/"+temp_path.split("/")[-1])
+        return
+    def __viz_annos_per_img(self,img_path, anno_df, target_path):
+        im = np.array(Image.open(img_path))
+        # Create figure and axes
+        fig,ax = plt.subplots(1)
+
+        # Display the image
+        ax.imshow(im)
+
+        for i,anno in anno_df.iterrows():
+            # Create a Rectangle patch
+            rect = patches.Rectangle((anno["x"],anno["y"]),anno["width"],anno["height"],linewidth=1,edgecolor='g',facecolor='none', label =str(anno["score"]))
+            # Add the patch to the Axes
+            ax.add_patch(rect)
+            ax.text(anno["x"], anno["y"], f'({int(np.around(anno["score"], decimals=2)*100)})', color='r', fontsize = 4)
+        #plt.show()
+        plt.savefig(target_path, dpi=250)
+        plt.close()
+        return
+    
+    def __write_coco(self):
+        self.__create_infos()
+        self.__create_licenses()
+        self.__create_categories()
+        self.__train_val_split()
+        self.train_img_df, self.train_anno_df, self.train_img_json,\
+        self.train_anno_json = self.__get_annos_and_img_metadata(self.__train)
+        
+        self.val_img_df, self.val_anno_df, self.val_img_json,\
+        self.val_anno_json = self.__get_annos_and_img_metadata(self.__val)
+        
+        self.__create_full_json(self.info_json, self.licenses_json, self.cat_json,\
+        self.train_img_json,self.train_anno_json, "instances_train.json")
+
+        self.__create_full_json(self.info_json, self.licenses_json, self.cat_json,\
+        self.val_img_json,self.val_anno_json, "instances_val.json")
+
+        return
+    
+    def __train_val_split(self):
+        imgs = self.results["img_pth"].unique()
+        train_imgs = imgs[:int(np.around(len(imgs)/2))]
+        
+        self.__train = self.results.loc\
+        [self.results["img_pth"].isin(train_imgs)].copy()
+        
+        self.__val = self.results.loc\
+        [~self.results["img_pth"].isin(train_imgs)].copy()
+        
+        for pth in tqdm(imgs):
+            if pth in train_imgs:
+                copy2(pth, self.session+"coco_annos/train/")
+            else:
+                copy2(pth, self.session+"coco_annos/val/")
+        return
+    
+    def __create_infos(self):
+        #Infoteil
+        self.info_dic={"year":datetime.datetime.today().year,\
+        "version":"",\
+        "description":f"Auto Generated Annotations. Project_Name {self.p}",\
+        "contributor":"", "url":"", "date_created":datetime.datetime.today()}
+        self.info_json=json.dumps(self.info_dic, default=str) 
+        return
+    
+    def __create_licenses(self):
+        #Infoteil
+        self.licenses_list=[{"id":1, "name":"Free Licence", "url":""}]
+        self.licenses_json=json.dumps(self.licenses_list, default=str) 
+        return
+    
+    def __create_categories(self):
+        #Infoteil
+        self.cat_list = []
+        for x in self.p_params["obj_list"]:    
+            self.cat_list.append({"id":self.p_params["obj_list"].index(x)+1\
+                             , "name":x, "supercategory":""})
+        self.cat_json=json.dumps(self.cat_list)
+        return
+    
+   
+    
+    def __generate_img_metadata(self, df):
+        df = df.drop(columns = \
+        ["score", "x","y","width","height","bbox","category_id"])
+        df = df.drop_duplicates()
+        df["file_name"] = df["img_pth"].str.split("/").str[-1]
+        df["width"] = df["img_pth"].apply(lambda x: Image.open(x).width)
+        df["height"] = df["img_pth"].apply(lambda x: Image.open(x).height)
+        df["license"] = 1
+        df = df.reset_index(drop=True).reset_index().rename(columns={"index":"id"})
+        return df
+    
+    def __process_annos(self,df):
+        df["area"] = df["width"] * df["height"]
+        df = df.drop(columns=["x","y","width","height","score"])
+        df["iscrowd"] = 0
+        df["segmentation"]=np.empty((len(df), 0)).tolist()
+        df = df.reset_index(drop=True).reset_index().rename(columns={"index":"id"})
+        return df
+    
+    def __replace_pth_with_id(self,imdf, annodf):
+        annodf = annodf\
+        .merge(imdf.rename(columns={"id":"image_id"})\
+        .loc[:,["img_pth","image_id"]], how="left", on=["img_pth"])\
+        .drop(columns=["img_pth"])
+        imdf = imdf.drop(columns=["img_pth"])
+        return imdf, annodf
+    
+    def __get_annos_and_img_metadata(self,data):
+        imgs_df = self.__generate_img_metadata(data)
+        anno_df = self.__process_annos(data)
+        imgs_df, anno_df = self.__replace_pth_with_id(imgs_df, anno_df)
+        return imgs_df, anno_df, imgs_df.to_json(orient="records"),\
+        anno_df.to_json(orient="records")
+    
+    def __create_full_json(self,info, licenses, cat, images, annos, filename):
+        full_json = {"info": json.loads(info), \
+        "licenses" :   json.loads(licenses),\
+        "categories":  json.loads(cat)  , \
+        "images":  json.loads(images)  , \
+        "annotations" :  json.loads(annos)  , \
+                    }
+        with open(self.session+"coco_annos/annotations/"+filename, "w")\
+        as outfile:
+            json.dump(full_json, outfile)
+        return
+    
+if __name__ == "__main__":
+    args = parse_arguments()
+    myFishPred = FishPred(args)
+    myFishPred.do_command()
