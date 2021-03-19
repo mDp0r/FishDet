9347404853c3e50cc93613d99eb5b01b93b18159
Mario <mDp0r@github.com>
2021-03-19 13:04:02 +0100

    Added lost creator to setup ds

--- setup_ds.py
+++ setup_ds.py
@@ -6,8 +6,13 @@ import json
 from shutil import copytree, copy2, rmtree
 import urllib.request
 import tarfile
-import argparse
+from tqdm import tqdm
+from copy import copy as copy_obj
 import yaml
+from ast import literal_eval
+import argparse
+import pandas as pd
+import datetime
 """
 Generell: 
 
@@ -233,6 +238,165 @@ class known_creator:
         rmtree("DeepFish" )
         return
 
+    
+class lost_creator:
+    def __init__(self, args):
+        self.args = copy_obj(args)
+        self.anno_file = args.anno_file
+        self.annos = pd.read_csv(self.args.path+ "annos_out/"+self.anno_file)
+        self.img_path = self.args.path + "imgs/"
+        return
+    
+    def __create_infos(self):
+        #Infoteil
+        self.info_dic={"year":datetime.datetime.today().year,\
+        "version":"",\
+        "description":f"Annotations generated from LOST. Project_Name {self.args.ds_name}",\
+        "contributor":"", "url":"", "date_created":datetime.datetime.today()}
+        self.info_json=json.dumps(self.info_dic, default=str) 
+        return
+    
+    def __create_licenses(self):
+        #Infoteil
+        self.licenses_list=[{"id":1, "name":"Free Licence", "url":""}]
+        self.licenses_json=json.dumps(self.licenses_list, default=str) 
+        return
+    
+    def __create_categories(self):
+        #Infoteil
+        self.cat_list = []
+        for label in self.annos.groupby(["anno.lbl.name", "anno.lbl.external_id"]).size().index:
+            self.cat_list.append({"id":label[1]\
+                             , "name":label[0], "supercategory":""})
+        self.cat_json=json.dumps(self.cat_list)
+        return
+    
+    def __train_val_split(self):
+        imgs = self.annos["img.img_path"].unique()
+        train_imgs = imgs[:int(np.around(len(imgs)/2))]
+        
+        self.train = self.annos.loc\
+        [self.annos["img.img_path"].isin(train_imgs)].copy().reset_index(drop = True)
+        
+        self.val = self.annos.loc\
+        [~self.annos["img.img_path"].isin(train_imgs)].copy().reset_index(drop = True)
+        
+
+        return
+
+    def __generate_img_metadata(self, df):
+        df = df.loc[:, ["file_name"]].copy()
+        df = df.drop_duplicates()
+        df["width"] = df["file_name"].apply(lambda x: Image.open(self.img_path + x).width)
+        df["height"] = df["file_name"].apply(lambda x: Image.open(self.img_path + x).height)
+        df["license"] = 1
+        df = df.reset_index(drop=True).reset_index().rename(columns={"index":"id"})
+        return df
+    
+    def __process_annos(self,df):
+        df = df.loc[:,["file_name", "width", "height", "bbox", "anno.lbl.external_id"]].copy()
+        df["area"] = df["width"].apply(lambda x: round(x)) * df["height"].apply(lambda x: round(x))
+        df = df.drop(columns=["width","height"])
+        df = df.rename(columns = {"anno.lbl.external_id": "category_id"})
+        df["iscrowd"] = 0
+        df["segmentation"]=np.empty((len(df), 0)).tolist()
+        df = df.reset_index(drop=True).reset_index().rename(columns={"index":"id"})
+        return df
+    
+    def __format_bbox_coco(self, annos):
+        annos["x"] = annos["anno.data"].apply(lambda x: literal_eval(x)["x"])
+        annos["y"] = annos["anno.data"].apply(lambda x: literal_eval(x)["y"])
+        annos["width"] = annos["anno.data"].apply(lambda x: literal_eval(x)["w"])
+        annos["height"] = annos["anno.data"].apply(lambda x: literal_eval(x)["h"])
+        annos["x"] = annos["x"].copy()-0.5*annos["width"].copy()
+        annos["y"] = annos["y"].copy()-0.5*annos["height"].copy()
+        annos["im_width"] = annos["file_name"].apply(lambda x: Image.open(self.img_path + x).width)
+        annos["im_height"] = annos["file_name"].apply(lambda x: Image.open(self.img_path + x).height)
+        annos["x"] = annos["x"].copy() * annos["im_width"].copy()
+        annos["width"] = annos["width"].copy() * annos["im_width"].copy()
+        annos["y"] = annos["y"].copy() * annos["im_height"].copy()
+        annos["height"] = annos["height"].copy() * annos["im_height"].copy()
+        annos["bbox"] = annos.apply(lambda x: [round(x["x"]), round(x["y"]), round(x["width"]),round(x["height"])] , axis=1)
+        return annos
+    
+    #labels
+    #Annos speichern
+    #Bilder kopieren
+    def __get_annos_and_img_metadata(self, data):
+        imgs_df = self.__generate_img_metadata(data)
+        anno_df = self.__process_annos(data)
+        imgs_df, anno_df = self.__replace_name_with_id(imgs_df, anno_df)
+        return imgs_df, anno_df, imgs_df.to_json(orient="records"),\
+        anno_df.to_json(orient="records")
+    
+    def __replace_name_with_id(self, imdf, annodf):
+        annodf = annodf\
+        .merge(imdf.rename(columns={"id":"image_id"})\
+        .loc[:,["file_name","image_id"]], how="left", on=["file_name"])\
+        .drop(columns=["file_name"])
+        return imdf, annodf
+    
+    def __create_temp_dir(self):
+        os.mkdir(self.args.ds_name)
+        os.mkdir(self.args.ds_name+ "/annotations")
+        os.mkdir(self.args.ds_name+ "/train")
+        os.mkdir(self.args.ds_name+ "/val")
+        return
+    def __create_full_json(self,info, licenses, cat, images, annos, filename):
+        full_json = {"info": json.loads(info), \
+        "licenses" :   json.loads(licenses),\
+        "categories":  json.loads(cat)  , \
+        "images":  json.loads(images)  , \
+        "annotations" :  json.loads(annos)  , \
+                    }
+        with open(self.args.ds_name+"/annotations/"+filename, "w")\
+        as outfile:
+            json.dump(full_json, outfile)
+        return
+    def __cleanup(self):
+        rmtree(self.args.ds_name)
+        return
+    
+    def __copy_images(self):
+            for idx, file_name in tqdm(self.train_img_df["file_name"].iteritems()):
+                    copy2(self.img_path+file_name, self.args.ds_name + "/train/")
+            for idx, file_name in tqdm(self.val_img_df["file_name"].iteritems()):
+                    copy2(self.img_path+file_name, self.args.ds_name + "/val/")
+    
+    def do_pipeline(self):
+        self.__create_temp_dir()
+        self.__create_infos()
+        self.__create_licenses()
+        self.annos["anno.lbl.name"] = self.annos["anno.lbl.name"].apply(lambda x: literal_eval(x)).str[0]
+        self.annos["anno.lbl.external_id"] = self.annos["anno.lbl.external_id"].apply(lambda x: literal_eval(x)).str[0]
+        self.annos = self.annos.loc[self.annos["anno.lbl.external_id"].notna()].copy()
+        self.annos["anno.lbl.external_id"].astype(int)
+        self.annos["file_name"] = self.annos["img.img_path"].str.split("/").str[-1]
+        self.__create_categories()
+        self.annos = self.__format_bbox_coco(self.annos)
+        self.__train_val_split()
+        
+        self.train_img_df, self.train_anno_df, self.train_img_json, self.train_anno_json = \
+        self.__get_annos_and_img_metadata(self.train)
+        
+        self.val_img_df, self.val_anno_df, self.val_img_json, self.val_anno_json = \
+        self.__get_annos_and_img_metadata(self.train)
+        
+        self.__create_full_json(self.info_json, self.licenses_json, self.cat_json,\
+        self.train_img_json, self.train_anno_json, "instances_train.json")
+        
+        self.__create_full_json(self.info_json, self.licenses_json, self.cat_json,\
+        self.val_img_json, self.val_anno_json, "instances_val.json")
+        
+        self.args.path = self.args.ds_name + "/"
+        self.__copy_images()
+        temp_coco_creator = coco_creator(self.args)
+        temp_coco_creator.do_pipeline()
+        self.__cleanup()
+        
+        return
+    
+
 
 def get_inputs():
     #Initialize parser
@@ -243,6 +407,9 @@ def get_inputs():
 default = None)
     parser.add_argument("--mode", action="store", dest="mode", type=str, \
 default = None)
+    #Only needed for LOST annos
+    parser.add_argument("--anno_file", action = "store", dest="anno_file", type=str, \
+    default = None)
     #Return parsed object
     return parser.parse_args()
 
@@ -258,6 +425,9 @@ if __name__ == "__main__":
         
     elif parsed_args.mode =="known":
         creator = known_creator(parsed_args)
+    
+    elif parsed_args.mode == "lost":
+        creator = lost_creator(parsed_args)
         
     else:
         print("No proper mode specified. Aborting")
